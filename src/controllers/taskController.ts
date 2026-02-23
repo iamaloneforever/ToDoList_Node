8db9d01f39d38e9b272a67f7bc389f27c5a7da32
AmirhoseinSharifi <amirhoseinsharifi@gmail.com>
2026-02-23 17:24:02 +0330

    Add zod and Add AddTask and FindTask

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
 import Task from "../schema/taskSchema";
+import { z } from "zod";
+
+const taskSchema = z.object({
+	title: z.string().min(1, "Title is required"), // Required field with minimum length
+	description: z.string().optional().default(""), // Optional field with default value
+	dueDate: z.coerce.date(), // Date validation
+	status: z
+		.enum(["pending", "in-progress", "done", "canceled"])
+		.default("pending"),
+});
+
+type Task = z.infer<typeof taskSchema>;
+
 export const GetTasks = async (req: Request, res: Response) => {
 	try {
 		const tasks = await Task.find();
@@ -8,6 +21,36 @@ export const GetTasks = async (req: Request, res: Response) => {
 		}
 		res.status(200).json(tasks);
 	} catch {
+		res.status(500).json({ Error: "Server Error" });
+	}
+};
+
+export const FindTask = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+		const task = await Task.findById(id);
+		if (!task) {
+			return res.status(404).json({ message: "Task not found" });
+		}
+
+		res.status(200).json(task);
+	} catch {
+		res.status(500).json({ Error: "Server Error" });
+	}
+};
+
+export const AddTask = async (req: Request, res: Response) => {
+	try {
+		const parsed = taskSchema.parse(req.body);
+		const task = new Task(parsed);
+		const savedTask = await task.save();
+		res.status(201).json(savedTask);
+	} catch (error) {
+		if (error instanceof z.ZodError) {
+			return res.status(400).json({
+				errors: error.format(), // Format errors for better readability
+			});
+		}
 		res.status(500).json({ Error: "Server Error" });
 	}
 };
--- src/routes/tasksRoutes.ts
+++ src/routes/tasksRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { GetTasks } from "../controllers/taskController";
+import { AddTask, FindTask, GetTasks } from "../controllers/taskController";
 
 const router = Router();
 
 router.get("/", GetTasks);
+router.get("/:id", FindTask);
+router.post("/addtask", AddTask);
 
 export default router;
